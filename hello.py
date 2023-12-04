497327eaf9677a55af4cc13b2e6531e8a2d001af
Narendra170901 <125501458+Narendra170901@users.noreply.github.com>
2023-12-04 18:51:08 -0500

    moved st.write down

--- hello.py
+++ hello.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd 
 import altair as alt
 
-data = pd.read_csv("Average_Daily_Traffic_Counts.csv")
-st.dataframe(data)
+
 
 
 # Streamlit app
@@ -17,3 +16,6 @@ bar_chart = alt.Chart(data).mark_bar().encode(
 
 # Display the Altair chart using st.altair_chart
 st.altair_chart(bar_chart, use_container_width=True)
+
+data = pd.read_csv("Average_Daily_Traffic_Counts.csv")
+st.dataframe(data)
